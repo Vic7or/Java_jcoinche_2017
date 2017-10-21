f592608ca8f54f1cb670c13393c8b8be2a9deef5
Victor Gsell <victor.gsell@epitech.eu>
2017-10-21 06:37:48 +0200

    ADDED GameManager to JcoincheServer AND extended Network with Protocol

--- src/main/java/JcoincheServer.java
+++ src/main/java/JcoincheServer.java
@@ -10,15 +10,34 @@ public class JcoincheServer {
     private static class ServerHolder {
         private final static JcoincheServer instance = new JcoincheServer();
     }
-    public static class Client extends Connection {
+    private static class GameManager extends Thread {
+        private static class GMHolder {
+            private final static GameManager instance = new GameManager();
+        }
+
+        public static GameManager getInstance() {
+            return GMHolder.instance;
+        }
+        private GameManager() {
+            System.out.println("GameManager instantiated.");
+        }
+        @Override
+        public void run(){
+            System.out.println("GameManager is running.");
+        }
+    }
+
+    private static class Client extends Connection {
         public String   name;
+        public int      points;
     }
     private final static Server         kryonet = new Server() {
         @Override
         protected Connection newConnection(){
             return new Client();
     }};
-    private final static List<Client> clientList = new ArrayList<Client>();
+    private final static List<Client>   clientList = new ArrayList<Client>();
+    private final static GameManager    gameManager = GameManager.getInstance();
     public static JcoincheServer getInstance() {
         return ServerHolder.instance;
     }
@@ -35,9 +54,12 @@ public class JcoincheServer {
                 if (JcoincheServer.clientList.size() < 4) {
                     System.out.println("new client connected ! =)");
                     JcoincheServer.clientList.add((Client) connection);
-                    JcoincheServer.kryonet.sendToAllTCP("Waiting for all players: " + String.valueOf(JcoincheServer.clientList.size()) + "/4...");
+                    JcoincheServer.kryonet.sendToAllTCP("Waiting for players: " + String.valueOf(JcoincheServer.clientList.size()) + "/4...");
                     if (JcoincheServer.clientList.size() == 4)
+                    {
                         JcoincheServer.kryonet.sendToAllTCP("Everyone is ready, the game starts!");
+                        JcoincheServer.gameManager.start();
+                    }
                 }
                 else {
                     JcoincheServer.kryonet.sendToTCP(connection.getID(), "Your connection has been rejected - Jcoinche server is full.");
@@ -54,7 +76,7 @@ public class JcoincheServer {
             }
             @Override
             public void disconnected(Connection connection){
-                System.out.println("a dummy client has left :( .");
+                System.out.println("Someone has left !");
                 for (ListIterator<Client> it = clientList.listIterator(); it.hasNext();)
                 {
                     if (it.next().getID() == connection.getID())
--- src/main/java/Network.java
+++ src/main/java/Network.java
@@ -4,8 +4,107 @@ import com.esotericsoftware.kryonet.EndPoint;
 public class Network {
     public final static int       ServerPort = 4243;
     public final static String    ServerIP = "192.168.1.25";
+    public enum Protocol {
+        WAITFORPLAYERS("Waiting for players: ", null){
+            @Override
+            public void print() {
+
+            }
+        },
+        REJECTCLIENT("Your connection has been rejected - Jcoinche server is full.", null) {
+            @Override
+            public void print() {
+
+            }
+        },
+        LEAVER("Someone has left !", null) {
+            @Override
+            public void print() {
+
+            }
+        },
+        READY("Everyone is ready, the game starts!", null) {
+            @Override
+            public void print() {
+
+            }
+        },
+        NEWTURN("\n\n\n------ Your turn begins! ------", null) {
+            @Override
+            public void print() {
+
+            }
+        },
+        ENDTURN("------ Your turn ends ------", null) {
+            @Override
+            public void print() {
+
+            }
+        },
+        ASKGAMBLE("Do you want to raise or fold for this gamble: ", null) {
+            @Override
+            public void print() {
+
+            }
+        },
+        RAISEGAMBLE("You raised the gamble to: ", null) {
+            @Override
+            public void print() {
+
+            }
+        },
+        FOLDGAMBLE("You folded.", null) {
+            @Override
+            public void print() {
+
+            }
+        },
+        ASKPLAYCARD("Which card do you want to play ?", null) {
+            @Override
+            public void print() {
+
+            }
+        },
+        PLAYCARD("You played the card: ", null){
+            @Override
+            public void print(){
+
+            }
+        },
+        CARDPLAYED(" played the card: ", null){
+            @Override
+            public void print() {
+
+            }
+        },
+        WHOISPLAYING(" is playing...", null){
+            @Override
+            public void print() {
+
+            }
+        };
+
+        private final String    what;
+        private Object          data;
+
+        Protocol(String id, Object object){
+            this.what = id;
+            this.data = object;
+        }
+        public void setData(Object object) {
+            this.data = object;
+        }
+        public Object getData() {
+            return this.data;
+        }
+        public String getWhat() {
+            return this.what;
+        }
+        public abstract void print();
+    }
     public static void register(EndPoint endp) {
         Kryo kryo = endp.getKryo();
         kryo.register(String.class);
+        kryo.register(Protocol.class);
     }
 }
